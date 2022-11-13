5b2120f97612bbdbc73d4d60ea0f5d803f0d5d64
SashaYaroshenko <yaroshenko2019ol@gmail.com>
2022-11-13 19:02:50 +0200

    Add VS project

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public class PerfectPower
+{
+    public static (int, int)? IsPerfectPower(int n)
+    {
+        if(n==0)
+        {
+            return null;
+        }
+        int basis = 2;
+        int power = 2;
+        double res = 0;
+        while(res!=n)
+        {
+            while (res < n)
+            {
+                res = Math.Pow(basis, power);
+                if (res != n)
+                {
+                    power++;
+                }
+                else
+                {
+                    return (basis, power);
+                }
+            }
+            basis++;
+            power = 2;
+            res = 0;
+            if(basis>=n)
+            {
+                return null;
+            }
+        }
+        return (basis,power);
+        
+    }
+}
